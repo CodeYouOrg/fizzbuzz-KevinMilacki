441f094bc92368c8b4e54e3a5ae458af224edc14
Kevin Milacki <kevin.milacki@gmail.com>
2024-07-24 17:53:02 -0400

    Wrote and finished FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,15 @@
 # add your code here
 
+nums = range(1, 100 + 1) 
+
+for num in nums:
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+    
